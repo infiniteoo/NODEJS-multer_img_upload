ae898a00f2907faeacbf3f26bd398b06535a5163
Troy Dorman <troydorman@gmail.com>
2021-07-19 22:13:30 -0600

    finally, display the uploaded image

--- app.js
+++ app.js
@@ -1,66 +1,76 @@
-const express = require('express');
-const multer = require('multer');
-const ejs = require('ejs');
-const path = require('path');
+const express = require("express");
+const multer = require("multer");
+const ejs = require("ejs");
+const path = require("path");
 
 // set storage engine
 const storage = multer.diskStorage({
-    destination: './public/uploads/',
-    filename: function(req, file, cb) {
-        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-    }
-})
+  destination: "./public/uploads/",
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
 
 // input upload
 const upload = multer({
-    storage: storage,
-    limits: {fileSize: 1000000},
-    fileFilter: function(req, file, cb){
-        checkFileType(file, cb);
-    }
-}).single('myImage');
+  storage: storage,
+  limits: { fileSize: 1000000 },
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  },
+}).single("myImage");
 
 // check file type
-function checkFileType(file, cb){
-     // Allowed ext
-     const filetypes = /jpeg|jpg|png|gif/;
-     // check ext
-    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-    // check mime type
-    const mimetype = filetypes.test(file.mimetype);
+function checkFileType(file, cb) {
+  // Allowed ext
+  const filetypes = /jpeg|jpg|png|gif/;
+  // check ext
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  // check mime type
+  const mimetype = filetypes.test(file.mimetype);
 
-    if(mimetype && extname) {
-        return cb(null, true);
-    } else {
-        cb('Error: Images Only!');
-    }
-};
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb("Error: Images Only!");
+  }
+}
 
-// init app 
+// init app
 const app = express();
 
-// EJS 
-app.set('view engine', 'ejs');
+// EJS
+app.set("view engine", "ejs");
 
 // PUBLIC FOLDER
-app.use(express.static('./public'));
+app.use(express.static("./public"));
 
 // ROUTES
-app.get('/', (req, res) => res.render('index'));
+app.get("/", (req, res) => res.render("index"));
 
-app.post('/upload', (req, res) => {
-    upload(req, res, (err) => {
-      if(err){
-          res.render('index', {
-              msg: err
-          });
+app.post("/upload", (req, res) => {
+  upload(req, res, (err) => {
+    if (err) {
+      res.render("index", {
+        msg: err,
+      });
+    } else {
+      if (req.file == undefined) {
+        res.render("index", {
+          msg: "error: no file selected!",
+        });
       } else {
-          console.log(req.file);
-          res.send('test');
+        res.render("index", {
+          msg: "file uploaded!",
+          file: `uploads/${req.file.filename}`,
+        });
       }
-    });
-})
+    }
+  });
+});
 const port = 3000;
 
 app.listen(port, () => console.log(`server started on port ${port}`));
-
