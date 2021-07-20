f35bbca7d7107a1ce4aa522bdc0368c79c46c9b9
Troy Dorman <troydorman@gmail.com>
2021-07-19 21:26:30 -0600

    wrote get route for /, defined EJS as our view engine, defined public folder and ensured index.ejs displays

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const path = require('path');
 // init app 
 const app = express();
 
+//EJS 
+app.set('view engine', 'ejs');
+
+// PUBLIC FOLDER
+app.use(express.static('./public'));
+
+app.get('/', (req, res) => res.render('index'));
+
 const port = 3000;
 
 app.listen(port, () => console.log(`server started on port ${port}`));
